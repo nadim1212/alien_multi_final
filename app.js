69698486a5b01d6c4117a18c63a4a7f10db477c3
nadim1212 <nadimniloy.swe@gmail.com>
2023-08-16 08:49:08 +0600

    server

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: 'https://alienbd.com',
+  origin: 'http://localhost:3000',
   credentials: true
 }));
 
